2eef10a2308b33b8de0449e8c60f0b59a486bd83
M1030516 <M1030516@A2ML17805.mindtree.com>
2018-04-28 23:18:50 +0530

    HotelBookingTest class issue fixed

--- codoingRound/src/main/java/HotelBookingTest.java
+++ codoingRound/src/main/java/HotelBookingTest.java
@@ -1,59 +1,56 @@
-//import com.sun.javafx.PlatformUtil;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.cleartrip.qa.base.TestBase;
 
 public class HotelBookingTest extends TestBase {
 
-    WebDriver driver = new ChromeDriver();
+	@FindBy(linkText = "Hotels")
+	private WebElement hotelLink;
 
-    @FindBy(linkText = "Hotels")
-    private WebElement hotelLink;
+	@FindBy(id = "Tags")
+	private WebElement localityTextBox;
 
-    @FindBy(id = "Tags")
-    private WebElement localityTextBox;
+	@FindBy(id = "SearchHotelsButton")
+	private WebElement searchButton;
 
-    @FindBy(id = "SearchHotelsButton")
-    private WebElement searchButton;
+	@FindBy(id = "travellersOnhome")
+	private WebElement travellerSelection;
 
-    @FindBy(id = "travellersOnhome")
-    private WebElement travellerSelection;
+	@BeforeClass
+	public void criarDriver() throws InterruptedException {
+		initialization();
+		PageFactory.initElements(driver, this);
+	}
 
-    @Test
-    public void shouldBeAbleToSearchForHotels() {
-        initialization();
+	@Test
+	public void shouldBeAbleToSearchForHotels() {
 
-        //driver.get("https://www.cleartrip.com/");
-        hotelLink.click();
+		System.out.println(hotelLink);
+		hotelLink.click();
 
-        localityTextBox.sendKeys("Indiranagar, Bangalore");
+		localityTextBox.sendKeys("Indiranagar, Bangalore");
 
-        new Select(travellerSelection).selectByVisibleText("1 room, 2 adults");
-        searchButton.click();
+		new Select(travellerSelection).selectByVisibleText("1 room, 2 adults");
+		searchButton.click();
 
-        driver.quit();
+		driver.quit();
 
-    }
-    
-   /* private void setDriverPath() {
-    	System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-    }
+	}
 
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
-    }*/
+	/*
+	 * private void setDriverPath() { System.setProperty("webdriver.chrome.driver",
+	 * "chromedriver.exe"); }
+	 * 
+	 * private void setDriverPath() { if (PlatformUtil.isMac()) {
+	 * System.setProperty("webdriver.chrome.driver", "chromedriver"); } if
+	 * (PlatformUtil.isWindows()) { System.setProperty("webdriver.chrome.driver",
+	 * "chromedriver.exe"); } if (PlatformUtil.isLinux()) {
+	 * System.setProperty("webdriver.chrome.driver", "chromedriver_linux"); } }
+	 */
 
 }
