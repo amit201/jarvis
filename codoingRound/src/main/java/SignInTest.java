49d23fdaba3b093c56a6969a10dd65be5c12002d
M1030516 <M1030516@A2ML17805.mindtree.com>
2018-04-28 22:24:40 +0530

    issue fixed in SignInTest class

--- codoingRound/src/main/java/SignInTest.java
+++ codoingRound/src/main/java/SignInTest.java
@@ -1,55 +1,35 @@
-//import com.sun.javafx.PlatformUtil;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import com.cleartrip.qa.base.TestBase;
 
 public class SignInTest extends TestBase {
+	@Test
+	public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
 
-    WebDriver driver = new ChromeDriver();
+		initialization();
+		waitFor(2000);
 
-    @Test
-    public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
+		driver.findElement(By.linkText("Your trips")).click();
+		driver.findElement(By.id("SignIn")).click();
 
-        initialization();
-        waitFor(2000);
+		WebElement signInOverlay = driver.findElement(By.cssSelector("[id='modal_window']"));
+		driver.switchTo().frame(signInOverlay);
 
-        driver.findElement(By.linkText("Your trips")).click();
-        driver.findElement(By.id("SignIn")).click();
+		driver.findElement(By.id("signInButton")).click();
 
-        driver.findElement(By.id("signInButton")).click();
+		String errors1 = driver.findElement(By.id("errors1")).getText();
+		Assert.assertTrue(errors1.contains("There were errors in your submission"));
+		driver.quit();
+	}
 
-        String errors1 = driver.findElement(By.id("errors1")).getText();
-        Assert.assertTrue(errors1.contains("There were errors in your submission"));
-        driver.quit();
-    }
-
-    private void waitFor(int durationInMilliSeconds) {
-        try {
-            Thread.sleep(durationInMilliSeconds);
-        } catch (InterruptedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-    }
-    
-/*    private void setDriverPath() {
-    	System.setProperty("webdriver.chrome.driver", "/codoingRound/chromedriver.exe");
-    }
-
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
-    }*/
-
+	private void waitFor(int durationInMilliSeconds) {
+		try {
+			Thread.sleep(durationInMilliSeconds);
+		} catch (InterruptedException e) {
+			e.printStackTrace(); // To change body of catch statement use File | Settings | File Templates.
+		}
+	}
 
 }
